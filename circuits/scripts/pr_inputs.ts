56aea2ff99d4ec26f0005c5994e0115611f7e066
zkFriendly <zkfriendly@skiff.com>
2024-02-14 23:30:35 +0100

    feat: tum input generator

--- circuits/scripts/pr_inputs.ts
+++ circuits/scripts/pr_inputs.ts
@@ -4,22 +4,13 @@ import { verifyDKIMSignature } from "@zk-email/helpers/dist/dkim";
 import fs from "fs";
 import path from "path";
 
-const artifacts_dir = path.join(__dirname, "../artifacts");
+const artifacts_dir = path.join(__dirname, "../artifacts/pr");
 
-function stringFromArray(data: number[]) {
-  var count = data.length;
-  var str = "";
-
-  for (var index = 0; index < count; index += 1)
-    str += String.fromCharCode(data[index]);
-
-  return str;
+if (!fs.existsSync(artifacts_dir)) {
+  fs.mkdirSync(artifacts_dir, { recursive: true });
 }
 
-export async function generatePrMergedIntoMainCircuitInputs(
-  rawEmail: string,
-  owner: string
-) {
+export async function getInputs(rawEmail: string, owner: string) {
   const STRING_PRESELECTOR = "Merged #";
   const TO_SELECTOR = "to:";
   const MAX_HEADER_PADDED_BYTES = 2048;
@@ -59,10 +50,7 @@ export async function generatePrMergedIntoMainCircuitInputs(
   );
   const owner =
     "13255821893820536903335282929376140649646180444238593676033702344407594536519";
-  const inputJson = await generatePrMergedIntoMainCircuitInputs(
-    rawEmail,
-    owner
-  );
+  const inputJson = await getInputs(rawEmail, owner);
 
   // input json path in artifacts
   const inputJsonPath = path.join(artifacts_dir, "input.json");
