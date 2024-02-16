36578a3e8eba5a588f54f7c9e934c6b463f502f8
zkFriendly <zkfriendly@skiff.com>
2024-02-16 21:08:11 +0100

    feat: setup cowork ticket reimbursment contract

--- semaphore/apps/contracts/contracts/perks/Cowork.sol
+++ semaphore/apps/contracts/contracts/perks/Cowork.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.4;
 /// @title Devconnect Coworking Space ticket reimbursement contract
 /// @author zkfriendly
 /// @notice This contract is used to reimburse the ticket of the coworking space based on a given gatekeeper
-
+/// this contract allows for a one time reimbursement of the ticket price in the token
 contract Cowork {
     address public gateKeeper;
+    address public token; // reimbursement are paid in this token
+    uint256 public ticketPrice; // price of the ticket in token
 
-    constructor(address _gateKeeper) {
+    constructor(address _gateKeeper, address _token, uint256 _ticketPrice) {
         gateKeeper = _gateKeeper;
+        token = _token;
+        ticketPrice = _ticketPrice;
     }
 }
--- semaphore/apps/contracts/test/Cowork.ts
+++ semaphore/apps/contracts/test/Cowork.ts
@@ -1,10 +1,32 @@
 import { ethers, run } from "hardhat"
-import { Cowork, GateKeeper, PrVerifier } from "../build/typechain"
+import { Cowork, GateKeeper, IERC20__factory, PrVerifier } from "../build/typechain"
+import { setupGateKeeper } from "./utils"
+import { expect } from "chai"
+import { MockContract, deployMockContract } from "ethereum-waffle"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { BigNumber } from "ethers"
 
 describe("Cowork", async () => {
     let coworkContract: Cowork
     let gateKeeperContract: GateKeeper
-    let prVerifierContract: PrVerifier // needed for the gatekeeper
+    let token: MockContract
+    const ticketPrice: BigNumber = ethers.utils.parseEther("0.1")
+    let signer: SignerWithAddress
 
-    beforeEach(async () => {})
+    beforeEach(async () => {
+        ;[signer] = await ethers.getSigners()
+        const { gateKeeper } = await setupGateKeeper()
+        gateKeeperContract = gateKeeper
+        token = await deployMockContract(signer, IERC20__factory.abi)
+
+        const CoworkFactory = await ethers.getContractFactory("Cowork")
+        coworkContract = await CoworkFactory.deploy(gateKeeperContract.address, token.address, ticketPrice)
+    })
+
+    it("should have proper setup", async () => {
+        expect(coworkContract.address).to.be.properAddress
+        expect(await coworkContract.gateKeeper()).to.equal(gateKeeperContract.address)
+        expect(await coworkContract.token()).to.equal(token.address)
+        expect(await coworkContract.ticketPrice()).to.equal(ticketPrice)
+    })
 })
