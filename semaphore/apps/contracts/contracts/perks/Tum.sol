ed0aba2eace65617cf964efe1683754593ade3cb
zkFriendly <zkfriendly@skiff.com>
2024-02-17 00:58:02 +0100

    feat: tum ticket verifier

---
+++ semaphore/apps/contracts/contracts/perks/Tum.sol
@@ -0,0 +1,63 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+import "../interfaces/IGateKeeperMeta.sol";
+import "../interfaces/ITumVerifier.sol";
+
+interface IERC20 {
+    function transfer(address to, uint256 amount) external returns (bool);
+}
+
+/// @title Tum Blockchain conference ticket reimbursement contract
+/// @author zkfriendly
+/// @notice This contract is used to reimburse the ticket of the blockchain conference based on a given gatekeeper
+contract Tum {
+    address public gateKeeper;
+    address public token; // reimbursement are paid in this token
+    uint256 public ticketPrice; // price of the ticket in token
+    address public groth16verifier;
+
+    struct GateKeeperProof {
+        uint256 merkleTreeRoot;
+        uint256 nullifierHash;
+        uint256[8] proof;
+    }
+
+    struct Proof {
+        uint[2] _pA;
+        uint[2][2] _pB;
+        uint[2] _pC;
+        uint[3] _pubSignals;
+    }
+
+    event Claimed(address indexed contributor, uint256 amount);
+
+    error InvalidProof();
+
+    constructor(address _gateKeeper, address _token, uint256 _ticketPrice, address _verifier) {
+        gateKeeper = _gateKeeper;
+        token = _token;
+        ticketPrice = _ticketPrice;
+        groth16verifier = _verifier;
+    }
+
+    function claim(GateKeeperProof calldata gateKeeperProof, Proof calldata proof) external {
+        IGateKeeper(gateKeeper).validateContributorSignal( // reverts if not contributor
+            IGateKeeper.Signal(
+                uint256(keccak256(abi.encodePacked(address(this)))),
+                getScope(),
+                gateKeeperProof.merkleTreeRoot,
+                gateKeeperProof.nullifierHash,
+                gateKeeperProof.proof
+            )
+        );
+        if (!ITumVerifier(groth16verifier).verifyProof(proof._pA, proof._pB, proof._pC, proof._pubSignals))
+            revert InvalidProof();
+
+        require(IERC20(token).transfer(msg.sender, ticketPrice));
+        emit Claimed(msg.sender, ticketPrice);
+    }
+
+    function getScope() public view returns (uint256) {
+        return uint(keccak256(abi.encodePacked(address(this))));
+    }
+}
---
+++ semaphore/apps/contracts/test/Tum.ts
@@ -0,0 +1,80 @@
+import { ethers } from "hardhat"
+import { GateKeeper, IERC20__factory, Tum, TumVerifier } from "../build/typechain"
+import { setupGateKeeper } from "./utils"
+import { expect } from "chai"
+import { MockContract, deployMockContract } from "ethereum-waffle"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { BigNumber } from "ethers"
+import { Identity } from "@semaphore-protocol/identity"
+import prProof from "./sample_proof/pr_proof.json"
+import tumProof from "./sample_proof/tum_proof.json"
+import { Group } from "@semaphore-protocol/group"
+import { generateProof } from "@semaphore-protocol/proof"
+import { config } from "../package.json"
+
+describe("Tum", async () => {
+    let tumContract: Tum
+    let gateKeeperContract: GateKeeper
+    let tumVerifier: TumVerifier
+    let token: MockContract
+    const ticketPrice: BigNumber = ethers.utils.parseEther("0.1")
+    let signer: SignerWithAddress
+    const user = new Identity(
+        "0xf7c78877048e423e22cafb473dc6e1324ea9a9c2463ad7d7d58ca5ec98e94dfd751ab265e92acac16040d46a7ccfabd4f322ffe1bc9dc93144ed81735f04dc801c"
+    )
+    let gp: Group
+
+    const wasmFilePath = `${config.paths.build["snark-artifacts"]}/semaphore.wasm`
+    const zkeyFilePath = `${config.paths.build["snark-artifacts"]}/semaphore.zkey`
+
+    async function joinContributors(proof: any, commitment: bigint) {
+        await gateKeeperContract.joinContributors(proof)
+        gp.addMember(commitment)
+    }
+
+    beforeEach(async () => {
+        ;[signer] = await ethers.getSigners()
+        const { gateKeeper } = await setupGateKeeper()
+        gateKeeperContract = gateKeeper
+        token = await deployMockContract(signer, IERC20__factory.abi)
+        const TumVerifierFactory = await ethers.getContractFactory("TumVerifier")
+        tumVerifier = (await TumVerifierFactory.deploy()) as TumVerifier
+
+        const TumFactory = await ethers.getContractFactory("Tum")
+        tumContract = await TumFactory.deploy(
+            gateKeeperContract.address,
+            token.address,
+            ticketPrice,
+            tumVerifier.address
+        )
+
+        const gpId = await gateKeeperContract.CONTRIBUTORS_GROUP_ID()
+        //@ts-ignore
+        gp = new Group(gpId)
+    })
+
+    it("should have proper setup", async () => {
+        expect(tumContract.address).to.be.properAddress
+        expect(await tumContract.gateKeeper()).to.equal(gateKeeperContract.address)
+        expect(await tumContract.token()).to.equal(token.address)
+        expect(await tumContract.ticketPrice()).to.equal(ticketPrice)
+    })
+
+    it("should be able to claim with valid proof", async () => {
+        await joinContributors(prProof.calldata, user.commitment)
+        const rawSignal = ethers.utils.keccak256(tumContract.address)
+        const scope = await tumContract.getScope()
+        const commitmentProof = await generateProof(user, gp, scope, rawSignal, {
+            wasmFilePath,
+            zkeyFilePath
+        })
+        const gateKeeperProof = {
+            merkleTreeRoot: commitmentProof.merkleTreeRoot,
+            nullifierHash: commitmentProof.nullifierHash,
+            proof: commitmentProof.proof
+        }
+        await token.mock.transfer.withArgs(signer.address, ticketPrice).returns(true)
+        //@ts-ignore
+        await tumContract.connect(signer).claim(gateKeeperProof, tumProof.calldata)
+    })
+})
