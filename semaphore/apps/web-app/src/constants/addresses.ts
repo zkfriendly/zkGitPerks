6a2caff78d752dd7c7153bc78e047ee7aaf178bc
zkFriendly <zkfriendly@skiff.com>
2024-02-17 01:31:26 +0100

    feat: fetch correct semaphore address in useSemaphore hook

--- semaphore/apps/web-app/src/constants/addresses.ts
+++ semaphore/apps/web-app/src/constants/addresses.ts
@@ -4,3 +4,7 @@ import { AddressMap } from "../types"
 export const GATEKEEPER_CONTRACT_ADDRESS_MAP: AddressMap = {
     [sepolia.id]: "0xc48490eED0b79011258bCA9c3fB248D718B0371e"
 }
+
+export const SEMAPHORE_CONTRACT_ADDRESS_MAP: AddressMap = {
+    [sepolia.id]: "0x3889927F0B5Eb1a02C6E2C20b39a1Bd4EAd76131"
+}
--- semaphore/apps/web-app/src/hooks/useSemaphore.ts
+++ semaphore/apps/web-app/src/hooks/useSemaphore.ts
@@ -2,6 +2,8 @@ import { SemaphoreEthers } from "@semaphore-protocol/data"
 import { BigNumber, utils } from "ethers"
 import { useCallback, useState } from "react"
 import { SemaphoreContextType } from "../context/SemaphoreContext"
+import { useContractAddress } from "./useContractAddress"
+import { SEMAPHORE_CONTRACT_ADDRESS_MAP } from "../constants/addresses"
 
 const ethereumNetwork =
     // @ts-ignore
@@ -11,17 +13,15 @@ const ethereumNetwork =
           import.meta.env.VITE_DEFAULT_NETWORK
 
 export default function useSemaphore({ groupId }: { groupId: string }): SemaphoreContextType {
+    const semaphoreAddress = useContractAddress(SEMAPHORE_CONTRACT_ADDRESS_MAP)
     const [_users, setUsers] = useState<any[]>([])
     const [_feedback, setFeedback] = useState<string[]>([])
 
     const refreshUsers = useCallback(async (): Promise<void> => {
         const semaphore = new SemaphoreEthers(ethereumNetwork, {
-            // @ts-ignore
-            address: import.meta.env.VITE_SEMAPHORE_CONTRACT_ADDRESS
+            address: semaphoreAddress
         })
-
         const members = await semaphore.getGroupMembers(groupId)
-
         setUsers(members)
     }, [])
 
@@ -35,7 +35,7 @@ export default function useSemaphore({ groupId }: { groupId: string }): Semaphor
     const refreshFeedback = useCallback(async (): Promise<void> => {
         const semaphore = new SemaphoreEthers(ethereumNetwork, {
             // @ts-ignore
-            address: import.meta.env.VITE_SEMAPHORE_CONTRACT_ADDRESS
+            address: semaphoreAddress
         })
 
         const proofs = await semaphore.getGroupVerifiedProofs(groupId)
