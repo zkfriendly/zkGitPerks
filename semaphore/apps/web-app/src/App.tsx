4ebefd26e4d3e6032b3440c07a98aa211e81a5c8
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2024-02-17 23:31:31 +0100

    main page styling

--- semaphore/apps/web-app/src/App.tsx
+++ semaphore/apps/web-app/src/App.tsx
@@ -1,7 +1,8 @@
-import { Route, Routes, useLocation } from "react-router-dom"
-import React, { useEffect, useState } from "react"
-import { ChakraProvider, Container, HStack, Spinner, Stack, Text } from "@chakra-ui/react"
-import { ConnectButton } from "@rainbow-me/rainbowkit"
+import {Route, Routes, useLocation} from "react-router-dom"
+import React, {useEffect, useState} from "react"
+import {ChakraProvider, Container, HStack, Spinner, Stack, Text} from "@chakra-ui/react"
+import {ConnectButton} from "@rainbow-me/rainbowkit"
+import styled from "styled-components";
 import useSemaphore from "./hooks/useSemaphore"
 import SemaphoreContext from "./context/SemaphoreContext"
 import LogsContext from "./context/LogsContext"
@@ -9,11 +10,28 @@ import theme from "./styles/index"
 import IdentitiesPage from "./pages"
 import ProofsPage from "./pages/proofs"
 import GroupsPage from "./pages/groups"
-import { GROUP_ID } from "./constants"
+import {GROUP_ID} from "./constants"
+
+const PageContainerWrapper = styled.div`
+    height: 100vh;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding-bottom: 56px;
+`
+
+const PageContainer = ({children}: { children: React.ReactNode }) => {
+    return (
+        <PageContainerWrapper>
+            {children}
+        </PageContainerWrapper>
+    )
+}
 
 export default function App() {
     const location = useLocation()
-    const semaphore = useSemaphore({ groupId: GROUP_ID })
+    const semaphore = useSemaphore({groupId: GROUP_ID})
     const [_logs, setLogs] = useState<string>("")
 
     useEffect(() => {
@@ -23,42 +41,48 @@ export default function App() {
 
     return (
         <ChakraProvider theme={theme}>
-            <HStack align="center" justify="right" p="2">
-                <ConnectButton />
-            </HStack>
+            <PageContainer>
+                <HStack align="center" justify="right" p="2">
+                    <ConnectButton/>
+                </HStack>
 
-            <Container maxW="lg" flex="1" display="flex" alignItems="center">
-                <Stack py="8" display="flex" width="100%">
-                    <SemaphoreContext.Provider value={semaphore}>
-                        <LogsContext.Provider
-                            value={{
-                                _logs,
-                                setLogs
-                            }}
-                        >
-                            <Routes>
-                                <Route path="/" element={<IdentitiesPage />} />
-                                <Route path="/proofs" element={<ProofsPage />} />
-                                <Route path="/groups" element={<GroupsPage />} />
-                                <Route element={<>Not found</>} />
-                            </Routes>
-                        </LogsContext.Provider>
-                    </SemaphoreContext.Provider>
-                </Stack>
-            </Container>
+                <Container maxW="lg" flex="1" display="flex" alignItems="center">
+                    <Stack py="8" display="flex" width="100%">
+                        <SemaphoreContext.Provider value={semaphore}>
+                            <LogsContext.Provider
+                                value={{
+                                    _logs,
+                                    setLogs
+                                }}
+                            >
+                                <Routes>
+                                    <Route path="/" element={<IdentitiesPage/>}/>
+                                    <Route path="/proofs" element={<ProofsPage/>}/>
+                                    <Route path="/groups" element={<GroupsPage/>}/>
+                                    <Route element={<>Not found</>}/>
+                                </Routes>
+                            </LogsContext.Provider>
+                        </SemaphoreContext.Provider>
+                    </Stack>
+                </Container>
 
-            <HStack
-                flexBasis="56px"
-                borderTop="1px solid #8f9097"
-                backgroundColor="#DAE0FF"
-                align="center"
-                justify="center"
-                spacing="4"
-                p="4"
-            >
-                {_logs.endsWith("...") && <Spinner color="primary.400" />}
-                <Text fontWeight="bold">{_logs || `Current step: ${location.pathname}`}</Text>
-            </HStack>
+                <HStack
+                    position='fixed'
+                    bottom="0"
+                    right='0'
+                    left='0'
+                    flexBasis="56px"
+                    borderTop="1px solid #8f9097"
+                    backgroundColor="#DAE0FF"
+                    align="center"
+                    justify="center"
+                    spacing="4"
+                    p="4"
+                >
+                    {_logs.endsWith("...") && <Spinner color="primary.400"/>}
+                    <Text fontWeight="bold">{_logs || `Current step: ${location.pathname}`}</Text>
+                </HStack>
+            </PageContainer>
         </ChakraProvider>
     )
 }
