f7e963c336fd6dbffde2517485edf0183e90ccac
Ali Mahdiyar <alimahdiyar77@gmail.com>
2024-02-18 19:45:55 +0330

    refactor: cleanup webapp files

--- apps/web-app/src/hooks/useSemaphore.ts
+++ apps/web-app/src/hooks/useSemaphore.ts
@@ -4,7 +4,6 @@ import { SemaphoreContextType } from "../context/SemaphoreContext"
 
 export default function useSemaphore({ groupId }: { groupId: bigint | undefined }): SemaphoreContextType {
     const [_users, setUsers] = useState<string[] | null>(null)
-    const [_feedback, setFeedback] = useState<string[]>([])
 
     const refreshUsers = useCallback(async (): Promise<void> => {
         if (!groupId) return
@@ -21,23 +20,6 @@ export default function useSemaphore({ groupId }: { groupId: bigint | undefined 
         setUsers((prevValue) => (prevValue ? [...prevValue, user] : [user]))
     }, [])
 
-    const refreshFeedback = useCallback(async (): Promise<void> => {
-        // const semaphore = new SemaphoreSubgraph(ethereumNetwork, {
-        //     // @ts-ignore
-        //     address: semaphoreAddress
-        // })
-        //
-        // const proofs = await semaphore.getGroupVerifiedProofs(groupId)
-        //
-        // setFeedback(proofs.map(({ signal }: any) => utils.parseBytes32String(BigNumber.from(signal).toHexString())))
-    }, [])
-
-    const addFeedback = useCallback(
-        (feedback: string) => {
-            setFeedback([..._feedback, feedback])
-        },
-        [_feedback]
-    )
 
     useEffect(() => {
         refreshUsers()
@@ -45,10 +27,7 @@ export default function useSemaphore({ groupId }: { groupId: bigint | undefined 
 
     return {
         _users,
-        _feedback,
         refreshUsers,
         addUser,
-        refreshFeedback,
-        addFeedback
     }
 }
